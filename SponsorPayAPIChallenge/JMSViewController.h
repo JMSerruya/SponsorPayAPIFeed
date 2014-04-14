11939f8ad47a9dde35dbcf1a3015de413421e8ff
Juan Manuel Serruya <shuank@gmail.com>
2014-04-14 10:54:56 +0200

    added intial form with automatic scroll

--- SponsorPayAPIChallenge/JMSViewController.h
+++ SponsorPayAPIChallenge/JMSViewController.h
@@ -8,6 +8,13 @@
 
 #import <UIKit/UIKit.h>
 
-@interface JMSViewController : UIViewController
+@interface JMSViewController : UIViewController <UITextFieldDelegate>
+
+@property (strong, nonatomic) IBOutlet UITextField *apikeyTextField;
+@property (strong, nonatomic) IBOutlet UITextField *appidTextField;
+@property (strong, nonatomic) IBOutlet UITextField *pub0TextField;
+@property (strong, nonatomic) IBOutlet UIScrollView *scrollView;
+
+@property (strong, nonatomic) IBOutlet UITextField *uidTextField;
 
 @end
--- SponsorPayAPIChallenge/JMSViewController.m
+++ SponsorPayAPIChallenge/JMSViewController.m
@@ -17,6 +17,10 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
+    self.uidTextField.delegate = self;
+    self.apikeyTextField.delegate = self;
+    self.appidTextField.delegate = self;
+    self.pub0TextField.delegate = self;
 	// Do any additional setup after loading the view, typically from a nib.
 }
 
@@ -24,6 +28,35 @@
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+// called when textField start editting.
+- (void)textFieldDidBeginEditing:(UITextField *)textField
+{
+    //activeField = textField;
+    [self.scrollView setContentOffset:CGPointMake(0,textField.center.y-200) animated:YES];
+}
+
+
+
+// called when click on the retun button.
+- (BOOL)textFieldShouldReturn:(UITextField *)textField
+{
+    NSInteger nextTag = textField.tag + 1;
+    // Try to find next responder
+    UIResponder *nextResponder = [textField.superview viewWithTag:nextTag];
+
+    if (nextResponder) {
+        [self.scrollView setContentOffset:CGPointMake(0,textField.center.y-200) animated:YES];
+        // Found next responder, so set it.
+        [nextResponder becomeFirstResponder];
+    } else {
+        [self.scrollView setContentOffset:CGPointMake(0,0) animated:YES];
+        [textField resignFirstResponder];
+        return YES;
+    }
+
+    return NO;
 }
 
 @end
